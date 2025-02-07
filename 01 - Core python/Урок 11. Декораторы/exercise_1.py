a39f972a4a6fca32b39912029f0c1b5c8a5fa33a
Alex M <avm@sh-inc.ru>
2025-02-07 12:04:25 +0100

    add lesson 11 - доп материал

--- 01 - Core python/Урок 11. Декораторы/exercise_1.py
+++ 01 - Core python/Урок 11. Декораторы/exercise_1.py
@@ -17,16 +17,33 @@
 
 # 2. Создайте декоратор cache, который запоминает результаты выполнения функции для заданных аргументов и возвращает
 # этот результат при повторном вызове декорируемой функции с теми же аргументами.
+cache_store = {}
+def cache(func):
+    global cache_store
+    if func.__name__ not in cache_store:
+        cache_store[func.__name__] = {}
 
-# @cache
-# def fibonacci(n):
-#     if n in (0, 1):
-#         return n
-#     return fibonacci(n - 1) + fibonacci(n - 2)
-#
-# print(fibonacci(10))
+    def wrapper(*args, **kwargs):
+        global cache_store
+
+        key = args + tuple(kwargs.items())
+
+        if key not in cache_store[func.__name__]:
+            cache_store[func.__name__][key] = func(*args, **kwargs)
+
+        return cache_store[func.__name__][key]
+
+    return wrapper
+
+@cache
+def fibonacci(n):
+    if n in (0, 1):
+        return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+print(fibonacci(10))
 # # Вывод: 55
 #
-# print(fibonacci(10))
-# # Вывод: 55 (использует кеш)
+print(fibonacci(10))
+# Вывод: 55 (использует кеш)
 
