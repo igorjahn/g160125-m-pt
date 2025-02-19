8ebc5786254c23a4cb271ab198185835fb436b3f
Alex M <avm@sh-inc.ru>
2025-02-19 12:03:07 +0100

    add lesson 19

--- 01 - Core python/Урок 19. Библиотеки Data and Time, OS/exercise_1.py
+++ 01 - Core python/Урок 19. Библиотеки Data and Time, OS/exercise_1.py
@@ -1,4 +1,5 @@
 # Тема: Модуль datetime
+from datetime import datetime
 
 # Задача 1: Определение текущей даты и времени
 # Напишите программу, которая выводит текущие дату и время в формате "YYYY-MM-DD HH:MM:SS".
@@ -8,6 +9,14 @@
 # Задача 2: Вычисление возраста
 # Напишите программу, которая принимает дату рождения пользователя в формате "YYYY-MM-DD" и вычисляет его возраст.
 
+# bt = input("Enter your birth date in format 'YYYY-MM-DD': ")
+# bt = datetime.strptime(bt, "%Y-%m-%d")
+# now = datetime.now()
+# age = now.year - bt.year
+# if now.month < bt.month or (now.month == bt.month and now.day < bt.day):
+#     age -= 1
+# print(f"Your age is {age}")
+#
 
 # Задача 3: Расчет дней до следующего мероприятия
 # Напишите программу, которая принимает дату следующего мероприятия в формате "YYYY-MM-DD" и выводит количество дней,
@@ -16,6 +25,10 @@
 
 # Задача 4: Определение дня недели
 # Напишите программу, которая принимает дату в формате "YYYY-MM-DD" и выводит день недели для этой даты.
+
+date = input("Enter date in format 'YYYY-MM-DD': ")
+date = datetime.strptime(date, "%Y-%m-%d")
+print(date.strftime("%A"))
 
 
 # Задача 5: Сравнение двух дат
--- 01 - Core python/Урок 19. Библиотеки Data and Time, OS/exercise_3.py
+++ 01 - Core python/Урок 19. Библиотеки Data and Time, OS/exercise_3.py
@@ -1,5 +1,6 @@
 # Тема: Интеграционная практика.
-
+import json
+import os
 # Проект: Перепишите проект из уроков 7-8, 14-15, добавив в него фичи 1 - 3 на основе модулей datetime и os.
 #
 # Фича 1. Добавьте в каждый товар дату и время его создания,
@@ -32,6 +33,194 @@
 # 7. Вывести список товаров меньше определенного количества.
 
 
+
+
+
+
+import time
+from datetime import datetime
+from json import JSONDecodeError
+map_count_call = {}
+DB = 'inventory.json'
+DB_COPY = 'inventory_copy.json'
+def retry_on_error(func):
+    global map_count_call
+    if func.__name__ not in map_count_call:
+        map_count_call[func.__name__] = 0
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except (FileNotFoundError, IOError, JSONDecodeError) as e:
+            time.sleep(10)
+        except Exception as e:
+            print(f"Ошибка: {e}")
+            map_count_call[func.__name__] += 1
+            if map_count_call[func.__name__] == 3:
+                print(f"Превышено количество попыток вызова функции {func.__name__}")
+                exit(1)
+            return wrapper(*args, **kwargs)
+        else:
+            map_count_call[func.__name__] = 0
+    return wrapper
+
+def log_it(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        with open('log.txt', 'a') as file:
+            file.write(f"{datetime.now()} Вызвана функция {func.__name__}. Результат: {result}\n")
+        return result
+    return wrapper
+
+@retry_on_error
+def save_inventory(inventory):
+    with open(DB, 'w') as file:
+        for el in inventory:
+            if 'created_at' in el:
+                el['created_at'] = el['created_at'].isoformat()
+            else:
+                el['created_at'] = datetime.now().isoformat()
+        json.dump(inventory, file)
+
+@retry_on_error
+def load_inventory():
+    # try:1
+    with open(DB) as file:
+        data = json.load(file)
+        for el in data:
+            if 'created_at' in el:
+                el['created_at'] = datetime.fromisoformat(el['created_at'])
+            else:
+                el['created_at'] = datetime.now()
+        return data
+    # except (FileNotFoundError, IOError, JSONDecodeError) as e:
+    #     print(f"Ошибка: {e}")
+    #     return []
+
+
+def show_inventory(inventory):
+    for product in inventory:
+        print_product(product)
+@log_it
+def add_product(inventory):
+    product = input("Enter product name: ")
+    try:
+        price = int(input("Enter product price: "))
+        count = int(input("Enter product count: "))
+    except ValueError as e:
+        print(f"Ошибка: {e}")
+        return add_product(inventory)
+    inventory.append({'product': product.title(), 'price': price, 'count': count, "created_at": datetime.now()})
+    return inventory
+@log_it
+def remove_product(inventory):
+    product = input("Enter product name: ")
+    for item in inventory:
+        if item['product'].lower() == product.lower():
+            inventory.remove(item)
+    return inventory
+@log_it
+def edit_product(inventory):
+    product = input("Enter product name: ")
+    for item in inventory:
+        if item['product'].lower() == product.lower():
+            try:
+                new_product = input(f"Enter new product name or {item['product']}: ")
+                if new_product:
+                    item['product'] = new_product.title()
+                new_price = input(f"Enter new product price or {item['price']}: ")
+                if new_price:
+                    item['price'] = int(new_price) * 0.1
+                new_count = input(f"Enter new product count or {item['count']}: ")
+                if new_count:
+                    item['count'] = int(new_count)
+            except ValueError as e:
+                print(f"Ошибка: {e}")
+                return edit_product(inventory)
+    return inventory
+
+def find_product(inventory):
+    product = input("Enter product name: ")
+    for item in inventory:
+        if item['product'].lower() == product.lower():
+            print_product(item)
+
+def find_product_min_cost(inventory):
+    try:
+        price = int(input("Enter price: "))
+    except ValueError as e:
+        print(f"Ошибка: {e}")
+        return find_product_min_cost(inventory)
+    for item in inventory:
+        if item['price'] <= price:
+            print_product(item)
+
+def print_product(product):
+    print(f"Product: {product['product']} Price: {product['price']} Count: {product['count']} Created at: {product['created_at']}")
+
+def find_product_min_count(inventory):
+    try:
+        count = int(input("Enter count: "))
+    except ValueError as e:
+        print(f"Ошибка: {e}")
+        return find_product_min_count(inventory)
+    for item in inventory:
+        if item['count'] <= count:
+            print_product(item)
+
+def copy_file(origin_path, new_path):
+    def read_file_by_cunck(file, size=1024):
+        while True:
+            data = file.read(size)
+            if not data:
+                break
+            yield data
+    def write_file_by_cunck(file, data):
+        for chunk in data:
+            file.write(chunk)
+    for el in read_file_by_cunck(open(origin_path, 'rb')):
+        with open(new_path, 'wb') as file:
+            write_file_by_cunck(file, el)
+
+
+while True:
+    inventory = load_inventory()
+    user_input = input(
+        "1. Показать список товаров.\n"
+        "2. Добавить товар.\n"
+        "3. Удалить товар.\n"
+        "4. Обновить название товара, стоимость или количество.\n"
+        "5. Найти товар по названию.\n"
+        "6. Вывести список товаров меньше определнной стоимости.\n"
+        "7. Вывести список товаров меньше определенного количества.\n"
+        "8. Выйти.\n"
+    )
+    match user_input:
+        case "1":
+            show_inventory(inventory)
+        case "2":
+            inventory = add_product(inventory)
+            save_inventory(inventory)
+        case "3":
+            inventory = remove_product(inventory)
+            save_inventory(inventory)
+        case "4":
+            inventory = edit_product(inventory)
+            save_inventory(inventory)
+        case "5":
+            find_product(inventory)
+        case "6":
+            find_product_min_cost(inventory)
+        case "7":
+            find_product_min_count(inventory)
+        case "8":
+            if os.path.exists(DB_COPY):
+                os.unlink(DB_COPY)
+            with open(DB, 'r') as file:
+                with open(DB_COPY, 'w') as file_copy:
+                    file_copy.write(file.read())
+            break
+        case _:
+            print("Invalid input")
 
 
 
