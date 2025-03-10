e75e0c1eaa994f8d8a71ed36916a879785cc9404
Alexandr Meshchriakov <avm@sh-inc.ru>
2025-03-10 10:13:52 +0100

    feat: добавление классов и функций для сериализации объектов в формат JSON

--- Модуль 3. Advanced Python/Урок 11. Библиотека JSON/src/Exercises/json-files.py
+++ Модуль 3. Advanced Python/Урок 11. Библиотека JSON/src/Exercises/json-files.py
@@ -1,11 +1,29 @@
-# Напишите код сериализации/десериализации списка и словаря Pyhton в формат строки JSON
-
 import json
+from icecream import ic as print
+
+def dict_to_json(python_object: dict, file):
+    json.dump(python_object, file)
+
+
+def json_to_dict(file: str) -> dict:
+    return json.load(file)
 
 
 def main():
-    # You code here
-    pass
+    # Пример объекта Python
+    data = {
+        'name': 'John',
+        'age': 30,
+        'city': 'New York'
+    }
+
+    # Преобразование объекта Python в строку JSON
+    with open('data.json', 'w') as file:
+        dict_to_json(data, file)
+
+    with open('data.json', 'r') as file:
+        python_object = json_to_dict(file)
+        print(python_object)
 
 
 if __name__ == '__main__':
